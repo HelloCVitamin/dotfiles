6301fa6b7e518d455b36e8721451a64a1953843d
Adrien Lescourt <adrien.lescourt@gmail.com>
2017-02-13 14:21:52 +0100

    Add missing PlugInstall in vim configuration

--- configure.sh
+++ configure.sh
@@ -75,6 +75,7 @@ ln -sv $PWD/vim $HOME/.vim
 #vim +PluginInstall +qall
 #Plug
 curl -fLo $PWD/vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall +qall
 cd $PWD/vim/plugged/Command-T/ruby/command-t
 ruby extconf.rb
 make
